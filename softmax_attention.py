b223543243e2a5ef4a7ac930ba572040adb9ec4c
Noah Farr <69793313+noahfarr@users.noreply.github.com>
2025-12-13 12:56:40 +0100

    Update softmax_attention.py

--- softmax_attention.py
+++ softmax_attention.py
@@ -9,57 +9,71 @@ def softmax_attention(Q_ptr, K_ptr, V_ptr, output_ptr,
                       K_stride_N, K_stride_d,
                       V_stride_N, V_stride_d,
                       output_stride_M, output_stride_d,
-                      BLOCKSIZE_M: tl.constexpr,
-                      BLOCKSIZE_N: tl.constexpr,
-                      BLOCKSIZE_d: tl.constexpr):
+                      BLOCK_SIZE_M: tl.constexpr,
+                      BLOCK_SIZE_N: tl.constexpr,
+                      BLOCK_SIZE_d: tl.constexpr):
 
     pid = tl.program_id(axis=0)
 
     offs_m = pid * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_d = tl.arange(0, BLOCK_SIZE_d)
 
-    Q_ptrs = Q_ptr + (offs_m[:, None] * Q_stride_m + offs_d[None, :] * Q_stride_d)
-    Q_mask = offs_m[:, None] < M
+    Q_ptrs = Q_ptr + (offs_m[:, None] * Q_stride_M + offs_d[None, :] * Q_stride_d)
+    Q_mask = (offs_m[:, None] < M) & (offs_d[None, :] < d)
     Q = tl.load(Q_ptrs, mask=Q_mask, other=0.0)
 
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_d), dtype=tl.float32)
     running_sum = tl.zeros([BLOCK_SIZE_M], dtype=tl.float32)
     running_max = tl.zeros([BLOCK_SIZE_M], dtype=tl.float32) - float("inf")
 
     for n in range(0, N, BLOCK_SIZE_N):
-        offs_n = n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-        K_ptrs = K_ptr + (offs_d[None, :] * K_stride_d + offs_n[:, None] * K_stride_n)
-        V_ptrs = V_ptr + (offs_d[None, :] * V_stride_d + offs_n[:, None] * V_stride_n)
-            
-        K = tl.load(K_ptrs, mask=offs_n[None, :] < N)
-        V = tl.load(V_ptrs, mask=offs_n[None, :] < N)
+        offs_n = n + tl.arange(0, BLOCK_SIZE_N)
         
-        K_ptrs += BLOCK_SIZE_N * K_stride_n
-        V_ptrs += BLOCK_SIZE_N * V_stride_n
+        K_ptrs = K_ptr + (offs_d[None, :] * K_stride_d + offs_n[:, None] * K_stride_N)
+        V_ptrs = V_ptr + (offs_d[None, :] * V_stride_d + offs_n[:, None] * V_stride_N)
+            
+        mask_K = (offs_n[:, None] < N) & (offs_d[None, :] < d)
+        mask_V = (offs_n[:, None] < N) & (offs_d[None, :] < d)
+        
+        K = tl.load(K_ptrs, mask=mask_K, other=0.0)
+        V = tl.load(V_ptrs, mask=mask_V, other=0.0)
 
-        qk = tl.dot(Q, tl.trans(K)) / tl.sqrt(d)
+        qk = tl.dot(Q, tl.trans(K)) / tl.sqrt(d.to(tl.float32))
+        qk = tl.where(offs_n[None, :] < N, qk, float("-inf"))
 
         max = tl.max(qk, axis=1)
-        logits = tl.exp(qk - m_j[:, None])
+        logits = tl.exp(qk - max[:, None])
         sum = tl.sum(logits, 1)
-        running_max = tl.maxiumum(running_max, max)
+
+        difference = running_max - tl.maximum(running_max, max)
+        running_max = tl.maximum(running_max, max)
+        alpha = tl.exp(difference)
+        beta = tl.exp(max - running_max)
+
+        running_sum = running_sum * alpha + sum * beta
+
+        qkv = tl.dot(logits, V)
+
+        accumulator = accumulator * alpha[:, None] + qkv * beta[:, None]
+
+    output = accumulator / running_sum[:, None]
+    
+    output_ptrs = output_ptr + (offs_m[:, None] * output_stride_M + offs_d[None, :] * output_stride_d)
+    output_mask = (offs_m[:, None] < M) & (offs_d[None, :] < d)
+    tl.store(output_ptrs, output, mask=output_mask)
 
 
-        qk = tl
-        qkv = tl.dot(qk, V)
-
-
-# Q, K, V, output are tensors on the GPU
 def solve(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, output: torch.Tensor, M: int, N: int, d: int):    
-    BLOCKSIZE_M = 16
-    BLOCKSIZE_d = 128
-    BLOCKSIZE_N = 64    
-    grid = (triton.cdiv(M, BLOCKSIZE_M), triton.cdiv(d, BLOCKSIZE_d))
+    BLOCK_SIZE_M = 16
+    BLOCK_SIZE_d = max(16, triton.next_power_of_2(d))
+    BLOCK_SIZE_N = 32
+
+    grid = (triton.cdiv(M, BLOCK_SIZE_M), )
     
     Q_stride_M, Q_stride_d = Q.stride()
     K_stride_N, K_stride_d = K.stride()
     V_stride_N, V_stride_d = V.stride()
-    out_stride_M, out_stride_d = output.stride()
+    output_stride_M, output_stride_d = output.stride()
     
     softmax_attention[grid](
         Q_ptr=Q,
@@ -81,7 +95,10 @@ def solve(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, output: torch.Tenso
         output_stride_M=output_stride_M,
         output_stride_d=output_stride_d,
         
-        BLOCKSIZE_M=BLOCKSIZE_M,
-        BLOCKSIZE_d=BLOCKSIZE_d,
-        BLOCKSIZE_N=BLOCKSIZE_N,
+        BLOCK_SIZE_M=BLOCK_SIZE_M,
+        BLOCK_SIZE_d=BLOCK_SIZE_d,
+        BLOCK_SIZE_N=BLOCK_SIZE_N,
+
+        num_stages=1,
+        num_warps=4
     )
